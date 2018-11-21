5233b7982dd46a1c79955aa872bd2e3c3adc19f8
Miguel Sanchez <misan@disca.upv.es>
2018-11-21 11:01:57 +0100

    Fix for ESP32

--- SDS011.h
+++ SDS011.h
@@ -21,9 +21,11 @@
 class SDS011 {
 	public:
 		SDS011(void);
-		void begin(uint8_t pin_rx, uint8_t pin_tx);
 		void begin(HardwareSerial* serial);
+#ifndef ESP32 
 		void begin(SoftwareSerial* serial);
+		void begin(uint8_t pin_rx, uint8_t pin_tx);
+#endif
 		int read(float *p25, float *p10);
 		void sleep();
 		void wakeup();
