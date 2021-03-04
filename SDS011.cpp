3ceaebfac695caf5c64fb72083f4c4430c46d8ec
Rajko Zschiegner <ricki_z@gmx.de>
2021-03-04 22:12:52 +0100

    add complete start cmd for newer SDS011

--- SDS011.cpp
+++ SDS011.cpp
@@ -10,7 +10,7 @@
 
 #include "SDS011.h"
 
-static const byte SLEEPCMD[19] = {
+static constexpr uint8_t SLEEPCMD[19] = {
 	0xAA,	// head
 	0xB4,	// command id
 	0x06,	// data byte 1
@@ -30,6 +30,15 @@ static const byte SLEEPCMD[19] = {
 	0xFF,	// data byte 15 (device id byte 2)
 	0x05,	// checksum
 	0xAB	// tail
+};
+static constexpr uint8_t WAKEUPCMD[19] = {
+	0xAA, 0xB4, 0x06, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x06, 0xAB
+};
+static constexpr uint8_t VERSION_CMD[] PROGMEM = {
+	0xAA, 0xB4, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x05, 0xAB
+};
+static constexpr uint8_t CONTINUOUS_MODE_CMD[] PROGMEM = {
+	0xAA, 0xB4, 0x08, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x07, 0xAB
 };
 
 SDS011::SDS011(void) {
@@ -92,8 +101,39 @@ void SDS011::sleep() {
 // SDS011:wakeup
 // --------------------------------------------------------
 void SDS011::wakeup() {
-	sds_data->write(0x01);
+	for (uint8_t i = 0; i < 19; i++) {
+		sds_data->write(WAKEUPCMD[i]);
+	}
 	sds_data->flush();
+	while (sds_data->available() > 0) {
+		sds_data->read();
+	}
+}
+
+// --------------------------------------------------------
+// SDS011:wakeup
+// --------------------------------------------------------
+void SDS011::wakeup() {
+	for (uint8_t i = 0; i < 19; i++) {
+		sds_data->write(WAKEUPCMD[i]);
+	}
+	sds_data->flush();
+	while (sds_data->available() > 0) {
+		sds_data->read();
+	}
+}
+
+// --------------------------------------------------------
+// SDS011:continuous_mode
+// --------------------------------------------------------
+void SDS011::continuous_mode() {
+	for (uint8_t i = 0; i < 19; i++) {
+		sds_data->write(CONTINUOUS_MODE_CMD[i]);
+	}
+	sds_data->flush();
+	while (sds_data->available() > 0) {
+		sds_data->read();
+	}
 }
 
 #ifndef ESP32
@@ -107,8 +147,14 @@ void SDS011::begin(uint8_t pin_rx, uint8_t pin_tx) {
 
 	sds_data = softSerial;
 }
+
+void SDS011::begin(SoftwareSerial* serial) {
+	serial->begin(9600);
+	sds_data = serial;
+}
 #endif
 
+#ifdef ESP32
 void SDS011::begin(HardwareSerial* serial) {
 	serial->begin(9600);
 	sds_data = serial;
@@ -116,12 +162,6 @@ void SDS011::begin(HardwareSerial* serial) {
 
 void SDS011::begin(HardwareSerial* serial, int8_t rxPin, int8_t txPin) {
 	serial->begin(9600, SERIAL_8N1, rxPin, txPin);
-	sds_data = serial;
-}
-
-#ifndef ESP32
-void SDS011::begin(SoftwareSerial* serial) {
-	serial->begin(9600);
 	sds_data = serial;
 }
 #endif
--- SDS011.h
+++ SDS011.h
@@ -21,15 +21,18 @@
 class SDS011 {
 	public:
 		SDS011(void);
-		void begin(HardwareSerial* serial);
-		void begin(HardwareSerial* serial, int8_t pin_rx, int8_t pin_tx);
 #ifndef ESP32 
 		void begin(SoftwareSerial* serial);
 		void begin(uint8_t pin_rx, uint8_t pin_tx);
 #endif
+#ifdef ESP32
+		void begin(HardwareSerial* serial);
+		void begin(HardwareSerial* serial, int8_t pin_rx, int8_t pin_tx);
+#endif ESP32
 		int read(float *p25, float *p10);
 		void sleep();
 		void wakeup();
+		void continuous_mode();
 	private:
 		uint8_t _pin_rx, _pin_tx;
 		Stream *sds_data;
